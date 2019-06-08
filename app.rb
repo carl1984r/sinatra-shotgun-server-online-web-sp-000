e9b533a9b7502e9020cd3e7fef4bb46bb9d18d79
Bry Cox <carl.cc@gmail.com>
2019-06-08 21:47:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! App was started with shotgun!."
+    "Welcome to your app!!!! App was started with shotgun!. Or not."
   end
 
 end
